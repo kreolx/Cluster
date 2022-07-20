68eef85fd5e4c1683f61140ba683d3d9990cca81
Dmitriy Korshikov <korshikovdn@gmail.com>
2022-07-20 13:28:45 +0400

    Добавить заготовку для ClusterMonitor

---
+++ Cluster/ClusterMonitor.cs
@@ -0,0 +1,12 @@
+﻿namespace Cluster
+{
+	public class ClusterMonitor
+	{
+		private readonly IClusterClient _client;
+
+		public ClusterMonitor(IClusterClient client)
+		{
+			_client = client;
+		}
+	}
+}
